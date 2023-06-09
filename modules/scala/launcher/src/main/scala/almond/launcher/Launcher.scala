02ff014748b628e5ebd6a2f6d8b005ea306268dc
Alex Archambault <alexandre.archambault@gmail.com>
2023-06-09 11:20:24 +0200

    Add logging / checks when spawning kernel in ITs

--- modules/scala/launcher/src/main/scala/almond/launcher/Launcher.scala
+++ modules/scala/launcher/src/main/scala/almond/launcher/Launcher.scala
@@ -179,6 +179,7 @@ object Launcher extends CaseApp[LauncherOptions] {
         setDaemon(true)
         override def run(): Unit =
           if (p.isAlive()) {
+            System.err.println("Shutting down underlying kernel")
             p.close()
             val timeout = 500.millis
             if (!p.waitFor(timeout.toMillis)) {
@@ -187,6 +188,7 @@ object Launcher extends CaseApp[LauncherOptions] {
               )
               p.destroyForcibly()
             }
+            System.err.println("Shut down underlying kernel")
           }
       }
     Runtime.getRuntime.addShutdownHook(hook)
